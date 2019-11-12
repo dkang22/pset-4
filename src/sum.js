4d852b1de461471f659e389eb2f2dde6457ed4a5
Danielle Kang <daniellekang24@gmail.com>
2019-11-12 10:47:32 -0500

    finished exercise 1

---
+++ src/sum.js
@@ -0,0 +1,23 @@
+const readlineSync = require("readline-sync");
+
+MIN = Number.MIN_SAFE_INTEGER;
+MAX = Number.MAX_SAFE_INTEGER;
+let sum = 0;
+
+console.log();
+do {
+    lowerValue = Number(readlineSync.question("Lower bound: "));
+    higherValue = Number(readlineSync.question("Upper bound: "));
+} while ((lowerValue < MIN || higherValue > MAX) || (lowerValue > higherValue));
+
+while (higherValue >= lowerValue) {
+    if (lowerValue % 2 === 0) {
+        sum += lowerValue;
+        lowerValue++;
+    } else {
+        lowerValue++;
+    }
+}
+
+sum = sum.toLocaleString("en");
+console.log("\n" + sum + ".");
