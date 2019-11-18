88d43ab08188966317b988a4bfa8a60266de07ab
Danielle Kang <daniellekang24@gmail.com>
2019-11-18 10:05:02 -0500

    progress

--- src/average.js
+++ src/average.js
@@ -3,8 +3,20 @@ const readlineSync = require("readline-sync");
 MIN = 0;
 MAX = Number.MAX_SAFE_INTEGER;
 let nonNegativeInteger;
+let numOfIntegersEntered = 0;
+let sum = 0;
+let average = 0;
 
 console.log();
 do {
     nonNegativeInteger = Number(readlineSync.question("Non-negative integer: "));
-} while (nonNegativeInteger < MIN || nonNegativeInteger > MAX);
+    if (nonNegativeInteger >= MIN && nonNegativeInteger < MAX) {
+        numOfIntegersEntered++;
+        sum += nonNegativeInteger;
+    }
+} while ((nonNegativeInteger >= MIN && nonNegativeInteger < MAX) || nonNegativeInteger > MAX);
+
+average = sum/numOfIntegersEntered;
+
+sum = sum.toLocaleString("en", {minimumFractionDigits: 3, maximumFractionDigits: 3 });
+console.log("\n" + sum + ".");
