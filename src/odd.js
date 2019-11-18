415f8ad58f37cce297ac2c0969726100d3154d38
Danielle Kang <daniellekang24@gmail.com>
2019-11-18 09:48:16 -0500

    finished 3

--- src/odd.js
+++ src/odd.js
@@ -1,10 +1,31 @@
 const readlineSync = require("readline-sync");
 
-MIN = Number.MIN_SAFE_INTEGER;
+MIN = 1;
 MAX = Number.MAX_SAFE_INTEGER;
 let posInteger;
+let sum = 0;
+let digit;
 
 console.log();
 do {
     posInteger = Number(readlineSync.question("Positive integer: "));
-} while (posInteger < MIN || posInteger > MAX);
+} while (posInteger < MIN || posInteger > MAX || Number.isNaN(posInteger));
+
+while (posInteger > 0) {
+    digit = posInteger % 10;
+    posInteger = Math.floor(posInteger/10);
+    switch (digit) {
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 9:
+          sum += digit;
+          break;
+        default:
+          sum += 0;
+          break;
+    }
+}
+
+console.log("\n" + sum + ".");
