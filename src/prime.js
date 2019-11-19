e4111a4ad8f95c96fecdd54e13278dc7f6fc030a
Danielle Kang <daniellekang24@gmail.com>
2019-11-19 11:45:46 -0500

    finished

--- src/prime.js
+++ src/prime.js
@@ -6,28 +6,29 @@ let nonNegativeInteger;
 let sumOfDigits = 0;
 let digit = 0;
 let lastDigit = 0;
+let digitsTester = 0;
 
 console.log();
 do {
     nonNegativeInteger = Number(readlineSync.question("Non-negative integer: "));
 } while (nonNegativeInteger < MIN || nonNegativeInteger > MAX || Number.isNaN(nonNegativeInteger));
 
+digitsTester = nonNegativeInteger;
+
 lastDigit = nonNegativeInteger % 10;
 
-while (nonNegativeInteger !== 0) {
-    digit = nonNegativeInteger % 10;
-    nonNegativeInteger = Math.floor(nonNegativeInteger/10);
+while (digitsTester !== 0) {
+    digit = digitsTester % 10;
+    digitsTester = Math.floor(digitsTester/10);
     sumOfDigits += digit;
 }
 
-if (sumOfDigits % 3 === 0) {
-    console.log("\nNot prime.");
-} else if (nonNegativeInteger === 2 || nonNegativeInteger === 3 || nonNegativeInteger === 5 || nonNegativeInteger === 7) {
+if (nonNegativeInteger == 2 || nonNegativeInteger == 3 || nonNegativeInteger == 5 || nonNegativeInteger == 7) {
     console.log("\nPrime.");
+} else if (sumOfDigits % 3 === 0) {
+    console.log("\nNot prime.");
 } else if (lastDigit % 2 === 0) {
     console.log("\nNot prime.");
 } else {
     console.log("\nPrime.");
 }
-
-//2 and 3 don't work
